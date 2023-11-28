5097fe0cebbf6f3fb481e62a496bc80c9cde3900
Roman <105315693+mokis3k@users.noreply.github.com>
2023-11-29 00:30:31 +0300

    completing functionality. elementary styles

--- script.js
+++ script.js
@@ -37,9 +37,6 @@ if (jokeForm) {
       const requestJoke = await fetch(API + path);
       if (!requestJoke.ok) throw new Error(requestJoke.status);
       const responseJoke = await requestJoke.json();
-      //   console.log(`status: ${requestJoke.status}`);
-      console.log(`joke: ${responseJoke.value}`);
-      //   console.dir(responseJoke);
 
       if (responseJoke.result) {
         responseJoke.result.forEach((joke) => renderJoke(joke));
@@ -78,13 +75,13 @@ const renderJoke = (joke) => {
       jokeInFavContainer.remove();
 
       const jokeInContainer = jokesContainer.querySelector(
-        `.joke__block[data-id="${joke.id}"]`
+        `.joke__block[data-id="joke_${joke.id}"]`
       );
       if (jokeInContainer) {
         const jokeInContainerFavBtn =
           jokeInContainer.querySelector(`.joke__fav__button`);
         jokeInContainerFavBtn.innerHTML = "💛";
-      } // Менять сердечко на кнопке не завивимо в контейнере она или нет.
+      }
     } else {
       joke.favourite = true;
       storageFavJokes.push(joke);
@@ -109,3 +106,49 @@ const renderJoke = (joke) => {
     ? jokesFavContainer.append(jokeBlock)
     : jokesContainer.append(jokeBlock);
 };
+
+const renderCategories = async () => {
+  try {
+    let requestCategories = await fetch(API + "/categories");
+    if (!requestCategories.ok) throw new Error(requestCategories.status);
+    let categories = await requestCategories.json();
+
+    let LIs = categories
+      .map(
+        (cat, index) =>
+          `<li class="joke__category">
+      <label>
+      ${cat} <input type="radio" name="jokeCategory" value="${cat}" ${
+            !index ? "checked" : ""
+          }>
+      </label>
+      </li>`
+      )
+      .join("");
+    jokeCategories.innerHTML = LIs;
+  } catch (status) {
+    console.log(status);
+  }
+};
+
+renderCategories();
+
+jokeTypes.forEach((input) => {
+  input.addEventListener("change", (e) => {
+    if (e.target.value === "categories") jokeCategories.classList.add("show");
+    else jokeCategories.classList.remove("show");
+
+    if (e.target.value === "search") jokeSearch.classList.add("show");
+    else jokeSearch.classList.remove("show");
+  });
+});
+
+const renderFavJokes = () => {
+  let storageFavJokes = localStorage.getItem("storageFavJokes");
+  if (storageFavJokes) {
+    storageFavJokes = JSON.parse(storageFavJokes);
+    storageFavJokes.forEach((joke) => renderJoke(joke));
+  }
+};
+
+renderFavJokes();
